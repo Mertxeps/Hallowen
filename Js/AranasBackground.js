4c47b5bf5ce4499595b550c8da4d05b72483528e
Mertxeps <mercedesps1968@gmail.com>
2025-11-04 17:49:42 +0100

    Creación HTML, CSS y Js de arañas animadas para todas las páginas

---
+++ Js/AranasBackground.js
@@ -0,0 +1,25 @@
+
+const numSpiders = 8;
+const spiderImg = "Images/Arañas/Araña.jpg";
+const container = document.getElementById('spider-background');
+
+for (let i = 0; i < numSpiders; i++) {
+    const spider = document.createElement("img");
+    spider.src = spiderImg;
+    spider.classList.add("spider");
+    container.appendChild(spider);
+    moveSpider(spider);
+}
+
+function moveSpider(spider) {
+    const x = Math.random() * (window.innerWidth - 100);
+    const y = Math.random() * (window.innerHeight - 100);
+    const rotation = Math.random() * 360;
+
+    spider.style.left = `${x}px`;
+    spider.style.top = `${y}px`;
+    spider.style.transform = `rotate(${rotation}deg)`;
+
+    const nextMove = 3000 + Math.random() * 3000;
+    setTimeout(() => moveSpider(spider), nextMove);
+}
